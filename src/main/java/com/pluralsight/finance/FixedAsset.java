81a2751ff97008804a85ebdab82c71184ab8e10a
lucas.rappatta24@outlook.com <lucas.rappatta24@outlook.com>
2025-11-04 10:53:38 -0600

    create getters, setters, field members, deposit/withdraw methods

--- src/main/java/com/pluralsight/finance/FixedAsset.java
+++ src/main/java/com/pluralsight/finance/FixedAsset.java
@@ -1,4 +1,21 @@
 package com.pluralsight.finance;
 
-public class FixedAsset {
+public abstract class FixedAsset {
+    private String name;
+    private double marketValue;
+
+    public FixedAsset(String name, double marketValue) {
+        this.name = name;
+        this.marketValue = marketValue;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getMarketValue() {
+        return marketValue;
+    }
+
+    public abstract double getValue();
 }
