7151f4bfcf9725a8befdeeb6191889d5c8ebf105
lucas.rappatta24@outlook.com <lucas.rappatta24@outlook.com>
2025-11-04 13:57:41 -0600

    add instances for each class in application and fix constructor in Portfolio

--- src/main/java/com/pluralsight/finance/FinanceApplication.java
+++ src/main/java/com/pluralsight/finance/FinanceApplication.java
@@ -1,4 +1,18 @@
 package com.pluralsight.finance;
 
 public class FinanceApplication {
+    public static void main(String[] args) {
+        CheckingAccount checkingAccount = new CheckingAccount("Lucas' Account", "123456789", 100000);
+        CreditCard creditCard = new CreditCard("Lucas' Quicksilver Card", "123456789", 999.99);
+        Gold gold = new Gold("Gold Chain", 50000, 50);
+
+        Portfolio portfolio = new Portfolio("Lucas' Assets", "Lucas");
+        portfolio.add(checkingAccount);
+        portfolio.add(creditCard);
+        portfolio.add(gold);
+
+        System.out.println(portfolio.getValue());
+
+
+    }
 }
